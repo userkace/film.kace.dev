83af71f5fa93a976d02d865812b76f38fb92030b
userkace <kerubinkuraido@gmail.com>
2024-04-19 04:21:44 +0800

    backend change
    moved to backend.sudo-flix.lol

--- .bkp/config.js
+++ .bkp/config.js
@@ -13,7 +13,7 @@ window.__CONFIG__ = {
   VITE_NORMAL_ROUTER: false,
 
   // The backend URL to communicate with
-  VITE_BACKEND_URL: "https://mw-backend.lonelil.com",
+  VITE_BACKEND_URL: "https://backend.sudo-flix.lol",
 
   // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
   VITE_DISALLOWED_IDS: "",
